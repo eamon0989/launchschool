f1985f2992102bd1a7697ec2a20d1c84df520bb4
Eamon <eamon0989@gmail.com>
2021-06-29 15:51:45 +0200

    finished lesson 4

--- js101/lesson_4/Practice Problem 11.js
+++ js101/lesson_4/Practice Problem 11.js
@@ -14,15 +14,46 @@
 // }
 // console.log(returnObj);
 
+// let statement = "The Flintstones Rock";
+// let returnObj = {};
+// statement.split('').filter(char => char !== ' ').forEach(ele => {
+//   if (!returnObj.hasOwnProperty(ele)) {
+//     returnObj[ele] = 1;
+//   } else {
+//     returnObj[ele] += 1;
+//   }
+// });
+
+// console.log(returnObj);
+/* 
 let statement = "The Flintstones Rock";
 let returnObj = {};
 statement.split('').filter(char => char !== ' ').forEach(ele => {
-  if (!returnObj.hasOwnProperty(ele)) {
-    returnObj[ele] = 1;
-  } else {
-    returnObj[ele] += 1;
-  }
+  returnObj[ele] = returnObj[ele] || 0;
+  returnObj[ele] += 1;
 });
 
 console.log(returnObj);
+ */
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+let array3 = [];
+console.log(array2.concat(array2[0]));
+// console.log(array2.slice(0, 1));
+// console.log(array2.splice(0, 1));
+console.log(array3);
+/* 
+arr.forEach(elem => {
+  arr.push(100);
+  console.log(elem);
+}); // 1, 2, 3, 4, 5, 6, 7
 
+let arr = [1, 2, 3, 4, 5, 6, 7];
+arr.forEach(elem => {
+  arr.splice(0, 1);
+  console.log(elem);
+}); // 1, 3, 5, 7
+
+let dog = 'fido';
+dog.replace('f', 'F');
+console.log(dog); */
