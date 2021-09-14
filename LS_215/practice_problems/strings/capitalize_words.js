05191c626e36fb1ecd70feb84ce37bb0c3156066
Eamon <eamon0989@gmail.com>
2021-09-14 13:32:56 +0200

    updated capitalize_words

--- LS_215/practice_problems/strings/capitalize_words.js
+++ LS_215/practice_problems/strings/capitalize_words.js
@@ -1,8 +1,17 @@
+// function wordCap(string) {
+//   return string.toLowerCase()
+//     .replace(/("\w)|(\b\w)/g, (_, p1, p2) => p1 ? p1 : p2.toUpperCase());
+// }
+
 function wordCap(string) {
   return string.toLowerCase()
-    .replace(/\b\w/g, match => match.toUpperCase());
+    .replace(/\b\w(?!\w+")/g, match => match.toUpperCase());
 }
+
 
 console.log(wordCap('four score and seven'));       // "Four Score And Seven"
 console.log(wordCap('the javaScript language'));    // "The Javascript Language"
 console.log(wordCap('this is a "quoted" word'));    // 'This Is A "quoted" Word'
+// console.log(wordCap(`this is a 'quoted' word`));    // 'This Is A "quoted" Word'
+
+
