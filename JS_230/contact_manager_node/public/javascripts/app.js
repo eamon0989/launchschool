9a60e9d8cb3fc4da2cb559cd39493bbd28e18307
Eamon <eamon0989@gmail.com>
2021-10-14 17:57:59 +0200

    started classbased refactor

--- JS_230/contact_manager_node/public/javascripts/app.js
+++ JS_230/contact_manager_node/public/javascripts/app.js
@@ -1,53 +1,20 @@
+import { contactsList, tagList } from '/javascripts/classes.js';
+// import tagList from '/javascripts/classes.js';
+
+// contactsList.createContact()
+// console.log(contact);
+
+console.log(contactsList);
+console.log(tagList);
+// tagList.createTag('buisness');
+// tagList.createTag('work');
+// console.log(tagList.getTag('buisness'));
+// console.log(tagList.getContactsWithTag('work'));
+
 document.addEventListener('DOMContentLoaded', e => {
-  let ul = document.getElementById('contacts-list');
-  let contacts = fetch('/api/contacts').then(res => res.json())
-    .then(contacts => {
-      // console.log(contacts);
-      contacts.forEach(contact => {
-        // console.log(contact);
-        // console.log(contact.email);
-        // console.log(contact.phone_number);
-        let li = document.createElement('li');
-        li.classList.add('contacts-list-items')
-        ul.appendChild(li);
-        let div = document.createElement('div');
-        let h3 = document.createElement('h3');
-        h3.textContent = contact['full_name'];
-        div.appendChild(h3);
-        li.appendChild(div);
-        let div2 = document.createElement('div');
-        li.appendChild(div2);
-        let dl = document.createElement('dl');
-        div2.appendChild(dl);
-        let dt = document.createElement('dt');
-        dt.textContent = 'Phone Number:';
-        let dd = document.createElement('dd');
-        dd.textContent = contact.phone_number;
-        let dt2 = document.createElement('dt');
-        dt2.textContent = 'Email:';
-        let dd2 = document.createElement('dd');
-        dd2.textContent = contact.email;
-        dl.appendChild(dt);
-        dl.appendChild(dd);
-        dl.appendChild(dt2);
-        dl.appendChild(dd2);
-        let div3= document.createElement('div');
-        li.appendChild(div3);
-        let editA = document.createElement('a');
-        editA.href = `/contacts/edit/${contact.id}`;
-        let editBtn = document.createElement('button'); 
-        editBtn.textContent = 'Edit';
-        editA.appendChild(editBtn);
-        let deleteA = document.createElement('a');
-        deleteA.href = `/contacts/delete/${contact.id}`;
-        let deleteBtn = document.createElement('button'); 
-        deleteBtn.textContent = 'Delete';
-        deleteA.appendChild(deleteBtn);
-        div3.appendChild(editA);
-        div3.appendChild(deleteA);
-        div3.classList.add('buttons-div');
-      })
-    })
+  let displayedContacts = 0;
+  let contactsArray = [];
+  // getAndDisplayContacts(contactsArray);
 
   let createForm = document.getElementById('create-form');
 
@@ -61,31 +28,114 @@ document.addEventListener('DOMContentLoaded', e => {
       json[prop[0]] = prop[1];
     }
 
-    let path = createForm.getAttribute('action');
-    let method = createForm.getAttribute('method');
-    fetch(path, { method: method, headers: { 'Content-Type': 'application/json' }, 
-      body: JSON.stringify(json)})
-      .then(res => console.log(res.status));
+    // console.log(json);
+
+    contactsList.createContact(json);
+
+    // let path = createForm.getAttribute('action');
+    // let method = createForm.getAttribute('method');
+    // fetch(path, { method: method, headers: { 'Content-Type': 'application/json' }, 
+    //   body: JSON.stringify(json)})
+    //   .then(res => res.json()).then(json => console.log(json));
   })
 
   document.addEventListener('click', e => {
     if (e.target.nodeName === 'BUTTON') {
-      e.preventDefault();
-    }
-
-    let editRegex = new RegExp('/contacts/edit/', 'gi');
-    if (editRegex.test(e.target.parentElement.href)) {
-      console.log(e.target.parentElement.href);
-    }
-
-    let deleteRegex = new RegExp('/contacts/delete/', 'gi');
-    if (deleteRegex.test(e.target.parentElement.href)) {
-      console.log(e.target.parentElement.href);
-      let alert = window.confirm('Are you sure you want to delete?');
-      if (alert) {
-        fetch (e.target.parentElement.href, { method: 'DELETE' });
+      let editRegex = new RegExp('/contacts/edit/', 'gi');
+      if (editRegex.test(e.target.parentElement.href)) {
+        console.log(e.target.parentElement.href);
+        e.preventDefault();
+  
       }
-    }
+  
+      let deleteRegex = new RegExp('/contacts/delete/', 'gi');
+      if (deleteRegex.test(e.target.parentElement.href)) {
+        e.preventDefault();
+        let alert = window.confirm('Are you sure you want to delete?');
+        if (alert) {
+          let id = e.target.parentElement.href.match(/\d+$/)[0];
+          fetch (`http://localhost:3000/api/contacts/${id}`, { method: 'DELETE' })
+            .then(res => {
+              if (res.status === 204) {
+                e.target.parentElement.parentElement.parentElement.remove();
+                displayedContacts -= 1;
+                if (displayedContacts < 1) {
+                  document.getElementById('display-contacts').style.display = 'none';
+                  document.getElementById('no-contacts').style.display ='flex';
+                }
+              }
+            });
+        }
+      }
 
+      if (e.target.textContent === 'Add Contact') {
+        // console.log('add contaxt');
+        document.getElementById('create-contact').style.display = 'flex';
+        document.getElementById('no-contacts').style.display ='none';
+
+        // console.log();
+      }
+
+      if (e.target.id === 'cancel-create-contact') {
+        document.getElementById('create-contact').style.display = 'none';
+        document.getElementById('no-contacts').style.display ='flex';      }
+    }
   })
 })
+
+// function createContactCard(contact) {
+//   let ul = document.getElementById('contacts-list');
+//   let li = document.createElement('li');
+//   li.classList.add('contacts-list-items')
+//   ul.appendChild(li);
+//   let div = document.createElement('div');
+//   let h3 = document.createElement('h3');
+//   h3.textContent = contact['full_name'];
+//   div.appendChild(h3);
+//   li.appendChild(div);
+//   let div2 = document.createElement('div');
+//   li.appendChild(div2);
+//   let dl = document.createElement('dl');
+//   div2.appendChild(dl);
+//   let dt = document.createElement('dt');
+//   dt.textContent = 'Phone Number:';
+//   let dd = document.createElement('dd');
+//   dd.textContent = contact.phone_number;
+//   let dt2 = document.createElement('dt');
+//   dt2.textContent = 'Email:';
+//   let dd2 = document.createElement('dd');
+//   dd2.textContent = contact.email;
+//   dl.appendChild(dt);
+//   dl.appendChild(dd);
+//   dl.appendChild(dt2);
+//   dl.appendChild(dd2);
+//   let div3= document.createElement('div');
+//   li.appendChild(div3);
+//   let editA = document.createElement('a');
+//   editA.href = `/contacts/edit/${contact.id}`;
+//   let editBtn = document.createElement('button'); 
+//   editBtn.textContent = 'Edit';
+//   editA.appendChild(editBtn);
+//   let deleteA = document.createElement('a');
+//   deleteA.href = `/contacts/delete/${contact.id}`;
+//   let deleteBtn = document.createElement('button'); 
+//   deleteBtn.textContent = 'Delete';
+//   deleteA.appendChild(deleteBtn);
+//   div3.appendChild(editA);
+//   div3.appendChild(deleteA);
+//   div3.classList.add('buttons-div');
+// }
+
+// function getAndDisplayContacts(contactsArray) {
+//   fetch('/api/contacts').then(res => res.json())
+//     .then(contacts => {
+//       if (contacts.length !== 0) {
+//         document.getElementById('display-contacts').style.display = 'block';
+//         document.getElementById('no-contacts').style.display ='none';
+//       }
+//       contacts.forEach(contact => {
+//         createContactCard(contact);
+//         contactsArray.push(contact);
+//       })
+//     })
+// }
